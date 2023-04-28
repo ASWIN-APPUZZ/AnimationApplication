4f158fe7350c46eca7e7843b832083cf1b31d0ba
aswin <aswin0484@gmail.com>
2023-04-28 10:38:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/animationapplication/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.animationapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+    Button blinkBTN, rotateBTN, fadeBTN, moveBTN, slideBTN, pulseBTN, stopBTN;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageview);
+        blinkBTN = findViewById(R.id.BTNblink);
+        rotateBTN = findViewById(R.id.BTNrotate);
+        fadeBTN = findViewById(R.id.BTNfade);
+        moveBTN = findViewById(R.id.BTNmove);
+        slideBTN = findViewById(R.id.BTNslide);
+        pulseBTN = findViewById(R.id.BTNpulse);
+        stopBTN = findViewById(R.id.BTNstop);
+
+        blinkBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // To add blink animation
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.blink);
+                imageView.startAnimation(animation);
+            }
+        });
+
+        rotateBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // To add rotate animation
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate);
+                imageView.startAnimation(animation);
+            }
+        });
+        fadeBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // To add fade animation
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade);
+                imageView.startAnimation(animation);
+            }
+        });
+        moveBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // To add move animation
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.move);
+                imageView.startAnimation(animation);
+            }
+        });
+        slideBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // To add slide animation
+                Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.slide);
+                imageView.startAnimation(animation);
+            }
+        });
+        pulseBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),pulseanimationActivity.class));
+            }
+        });
+        stopBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // To stop the animation going on imageview
+                imageView.clearAnimation();
+            }
+        });
+    }
+}
